6eca7890e9a2e7968f31379e91b281c715c261da
gergodancs <dancs.gergely@gmail.com>
2022-02-28 08:56:39 +0100

    animated login

--- src/comps/Login.jsx
+++ src/comps/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/reducer";
+import { motion } from "framer-motion";
 import "../css/form.css";
 
 const Login = () => {
@@ -29,19 +30,34 @@ const Login = () => {
     <div className="login">
       <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
         <h1>login here</h1>
-        <input
+        <motion.input
+          whileHover={{
+            scale: 0.9,
+            transition: { type: "spring" },
+            duration: 0.1,
+          }}
           onChange={(e) => setName(e.target.value)}
           value={name}
           type="name"
           placeholder="name"
         />
-        <input
+        <motion.input
+          whileHover={{
+            scale: 0.9,
+            transition: { type: "spring" },
+            duration: 0.1,
+          }}
           onChange={(e) => setEmail(e.target.value)}
           value={email}
           type="email"
           placeholder="email"
         />
-        <input
+        <motion.input
+          whileHover={{
+            scale: 0.9,
+            transition: { type: "spring" },
+            duration: 0.1,
+          }}
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           type="password"
